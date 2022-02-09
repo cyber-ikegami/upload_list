b64328f9f9bf4a77a3f6788de71d49e3d0f72e1f
cyber-ikegami <89112341+cyber-ikegami@users.noreply.github.com>
2022-02-09 16:15:03 +0900

    コマンドライン引数でディレクトリ指定()

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ const fs = require('fs');
 
 const dirPath = process.argv[2];
 const fileNameList = fs.readdirSync(dirPath);
-let fileName = "";
-for (let i = 0; i < fileNameList.length; i++) {
-    fileName = `${fileName}ファイル名：${fileNameList[i]}\n`;
-}
-console.log(fileName);
+
+fileNameList.forEach(function(fileName) {
+    console.log(`ファイル名：${fileName}`);
+});
