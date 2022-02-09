bce26aacabeced5fd60bb5cfd066c162576427be
cyber-ikegami <89112341+cyber-ikegami@users.noreply.github.com>
2022-02-09 14:42:33 +0900

    ファイル名出力（フォーマット）

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 
 const dirPath = 'C:/Users/ikegami/Desktop/テスト用';
-const fileName = fs.readdirSync(dirPath);
+const fileNameList = fs.readdirSync(dirPath);
+let fileName = "";
+for (let i = 0; i < fileNameList.length; i++) {
+    fileName = `${fileName}ファイル名：${fileNameList[i]}\n`;
+}
 console.log(fileName);
