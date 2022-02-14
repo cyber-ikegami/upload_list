93a62b3bec6ffe8dac72231dd7be8faa30e8b830
cyber-ikegami <89112341+cyber-ikegami@users.noreply.github.com>
2022-02-14 11:20:17 +0900

    typescriptに変更→コンパイル

--- main.ts
+++ main.ts
@@ -1,29 +1,32 @@
-"use strict";
 const fs = require('fs');
 const xlsx = require('xlsx');
+
 const ITEM_START_NUM = 3;
+
 // パス
 const dirPath = process.argv[2];
 // ファイル名の配列
 const fileNameList = fs.readdirSync(dirPath);
-fileNameList.forEach((fileName) => {
+
+fileNameList.forEach((fileName : string) => {
     // エクセルデータ
     const excelData = xlsx.readFile(`${dirPath}/${fileName}`);
     // シート名
     const sheetNames = excelData.SheetNames;
     // 機能名(シート1の名称 - 「フォーマット」)
     const funcName = sheetNames[0].replace('フォーマット', '');
+
     // 項目名のリスト
     let itemList = new Array();
     // エラーメッセージのリスト
     let errorMessageList = new Array();
+
     // エラーチェック(シート数の確認)
     if (sheetNames.length <= 1) {
         errorMessageList.push('エラー：シートが1つしか存在しません。');
-    }
-    else {
+    } else {
         const itemSheet = excelData.Sheets['入力データ項目'];
-        for (let i = ITEM_START_NUM;; i++) {
+        for (let i = ITEM_START_NUM; ; i++){
             const cellNum = `B${i}`;
             if (itemSheet[cellNum] == undefined) {
                 break;
@@ -31,11 +34,13 @@ fileNameList.forEach((fileName) => {
             const itemName = itemSheet[cellNum].v;
             itemList.push(`・${itemName}\n`);
         }
+
         // エラーチェック(入力データ項目のフォーマットの確認)
         if (itemList.length == 0) {
             errorMessageList.push('エラー：フォーマットが違います。');
         }
     }
+
     console.log(`★ファイル名：${fileName}、機能名：${funcName}
     ------------------------------------------------
     ${errorMessageList.length == 0 ? itemList : errorMessageList}
